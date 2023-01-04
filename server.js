de198fb6a04b76e40a5eb449c417bee7656dae81
Lesley-Byte <phosphorescence3@hotmail.com>
2023-01-04 08:31:08 -0500

    Adding code to update db.json file

--- server.js
+++ server.js
@@ -13,44 +13,45 @@ app.use(express.static('public'));
 // GET / should return the index.html file.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    console.log("get slash - line 16")
     }
 );
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+    console.log("get notes - line 22")
     }
 );
 // GET * should return the index.html file.
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+    console.log("get * - line 28")
     }
 );
 
 // `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => res.json(termData));
+app.get('/api/notes', (req, res) => {
+    res.json(termData);
+    console.log("get api notes - line 35")
+    }
+    );
+
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post('/api/notes', (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    const newNote = req.body;
-    console.log(newNote);
-    // We then add the json the user sent to the character array
-    termData.push(` newNote is: ${newNote}`);
-    console.log(` termData is: ${termData}`);
+    const newNote = (req.body);
+    console.log(req.body);
+    console.log(newNote + " - line 45");
+    // We then add the json the user sent to json
+    termData.push(newNote);
     // We then display the JSON to the users
     res.json(newNote);
-    // use fs to write to db.json
-    //create a variable to hold the stringify'd termData
-    const termDataString = res.json(newNote)
-    console.log(` termDataString is: ${termDataString}`);
-    // 
-    fs.writeFile('./db/db.json', termDataString, (err) => {
-        if (err) {
-            console.log(`Error writing to db.json: ${err}`);
-            }
-        else {
-            console.log(`Successfully wrote to db.json`);
-            }
+    console.log("post api notes - line 50")
+    // write to db.json using fs
+    fs.writeFile('./db/db.json', JSON.stringify(termData), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
         }
     );
     }
