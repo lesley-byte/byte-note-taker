a078329b555e312512db3f29d0234b64e8eec0c9
lesley-byte <phosphorescence3@hotmail.com>
2023-01-06 21:11:40 -0500

    now it deletes stuff

--- routes/notes.js
+++ routes/notes.js
@@ -1,31 +1,53 @@
-const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-    console.info(`${req.method} request received for notes`);
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// Delete route for a specific note
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
 });
 
 // POST Route for a new note
-notes.post('/', (req, res) => {
-    console.info(`${req.method} request received to add a note--this is in the notes.js file`);
+notes.post("/", (req, res) => {
+  console.info(
+    `${req.method} request received to add a note--this is in the notes.js file`
+  );
 
-    const { title, text } = req.body;
+  const { title, text } = req.body;
 
-    if (req.body) {
-        const newNote = {
-            title,
-            text,
-            note_id: uuidv4(),
-        };
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      //   note_id: uuidv4(),
+    };
 
-        readAndAppend(newNote, './db/db.json');
-        res.json(`Note added successfully 🚀`);
-    } else {
-        res.error('Error in adding note');
-    }
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error("Error in adding note");
+  }
 });
 
 module.exports = notes;
